d5065f5aa1d183667cc2ef697d681a4a6e9af79e
Hikari7 <h.kobe712@gmail.com>
2023-01-16 13:02:49 -0800

    added ejs files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 /* ---------------------------- setups and config --------------------------- */
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
+///
+app.set("view engine", "ejs");
+app.set("views", "src/views");
+///
 
 /* ------------------------------- middleware ------------------------------- */
-app.use('/api/members', require('./routes/members'))
+app.use("/api/members", require("./routes/members"));
 
-app.get('/', (req,res) => res.json({ msg: 'Health Check'}))
+// app.get("/", (req, res) => res.json({ msg: "Health Check" }));
+app.get("/", (req, res) => res.render("homepage", { title: "Homepage" }));
 
-app.use((req,res) => res.sendFile(path.join(__dirname, 'public', '404.html')))
+
+app.post("/api/members", (req, res, next) => {
+  console.log(req.body);
+  const { name, email } = req.body;
+  if (name === "admin" && email === "admin") {
+    res.render("members", {
+      title: `Welcome, ${name}`,
+      username: "",
+      name: "Member lists",
+    });
+  } else {
+    res.render("api/failure", { title: "Opps!" });
+  }
+
+  res.redirect("/");
+});
+
+app.use((req, res) => res.sendFile(path.join(__dirname, "public", "404.html")));
 
 /* -------------------------------- listener -------------------------------- */
-const PORT = process.env.PORT || 8000 
-app.listen(PORT)
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Serever is listening on ${PORT}🚀`));
--- src/routes/members.js
+++ src/routes/members.js
@@ -1,35 +1,40 @@
-const router = require('express').Router()
-const uuid = require("uuid")
+const router = require("express").Router();
+const uuid = require("uuid");
 
 const members = [
-    { id: uuid.v4(), name: "Mario", email: "mario@mail.com" },
-    { id: uuid.v4(), name: "Luigi", email: "luigi@mail.com" },
-    { id: uuid.v4(), name: "Yoshi", email: "yoshi@mail.com" },
-]
+  { id: uuid.v4(), name: "Mario", email: "mario@mail.com" },
+  { id: uuid.v4(), name: "Luigi", email: "luigi@mail.com" },
+  { id: uuid.v4(), name: "Yoshi", email: "yoshi@mail.com" },
+];
 
-router.get('/', (req, res) => res.json(members))
+router.get("/", (req, res) => res.json(members));
 
-router.get('/:id', (req, res) => {
-    const paramsID = req.params.id
-    
-    const found = members.some(member => member.id === paramsID)
-    
-    if(found){
-        res.json(members.filter(member => member.id === paramsID))
-    }else{
-        res.status(400).json({ msg: `Member with id: ${paramsID}, is not found `})
-    }
-})
+router.get("/:id", (req, res) => {
+  const paramsID = req.params.id;
 
-router.post('/', (req,res) => {
-    const newData = {
-        id: uuid.v4(),
-        name: req.body.name,
-        email: req.body.email
-    }
+  const found = members.some((member) => member.id === paramsID);
 
-    members.push(newData)
-    res.json(members)
-})
+  if (found) {
+    res.json(members.filter((member) => member.id === paramsID));
+  } else {
+    res.status(400).json({ msg: `Member with id: ${paramsID}, is not found ` });
+  }
+});
 
-module.exports = router
+router.post("/", (req, res) => {
+  const newData = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  members.push(newData);
+  //これの代わりにHope pageとかにリダイレクトさせて、そこにall membersを表示させる
+  //serving jsonの代わりにHTMLを返しましょう
+  //submitting form as well
+  res.json(members);
+
+  // res.send(req.body);
+});
+
+module.exports = router;
